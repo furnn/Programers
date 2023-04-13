89e78866957eab4684d23d2d84096b2e3afb6306
furnn <gka06566@gmail.com>
2023-04-13 19:36:38 +0900

    add files

--- month1/example.py
+++ month1/example.py
@@ -1,3 +1,18 @@
-aa={'a':3,'b':4,'c':5}
+import chart_studio
+import chart_studio.plotly as py
+import chart_studio.tools as tls
 
-print(aa.pop('a'), aa)
+from matplotlib import pyplot as plt
+import numpy as np
+import math 
+#needed for definition of pi
+x = np.arange(0, math.pi*2, 0.05)
+y = np.sin(x)
+plt.plot(x,y)
+plt.xlabel("angle")
+plt.ylabel("sine")
+plt.title('sine wave')
+
+fig = plt.gcf()
+plotly_fig = tls.mpl_to_plotly(fig)
+py.iplot(plotly_fig)
