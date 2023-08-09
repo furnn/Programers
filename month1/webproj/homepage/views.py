7aa24cca0554d6e25a7eaf94a165af7aa4d09083
furnn <gka06566@gmail.com>
2023-08-09 19:13:33 +0900

    file upload

--- month1/webproj/homepage/views.py
+++
@@ -1,8 +0,0 @@
-from http.client import HTTPResponse
-from django.shortcuts import HttpResponse, render
-
-# Create your views here.
-
-
-def index(request):
-    return render(request, 'index.html', {})
